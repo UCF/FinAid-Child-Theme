f57ad62aa9181efd8001cfd0c9b89a46e6a7881f
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-09-30 13:28:12 -0400

    Added acf paths and section layout fields

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 // Theme foundation
 include_once 'includes/config.php';
 include_once 'includes/meta.php';
+include_once 'includes/section-functions.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -9,4 +9,35 @@ define( 'FINAID_THEME_CSS_URL', FINAID_THEME_STATIC_URL . '/css' );
 define( 'FINAID_THEME_JS_URL', FINAID_THEME_STATIC_URL . '/js' );
 define( 'FINAID_THEME_IMG_URL', FINAID_THEME_STATIC_URL . '/img' );
 
+/**
+ * Adds the ACF folder within the child theme
+ * @author Jim Barnes
+ * @since 1.0.0
+ * @param string $path The path to the ACF folder
+ * @return string
+ */
+function add_acf_save_path( $path ) {
+	$path = get_stylesheet_directory() . '/dev/acf';
 
+	return $path;
+}
+
+add_filter( 'acf/settings/save_json', __NAMESPACE__ . '\add_acf_save_path', 10, 1 );
+
+/**
+ * Adds the ACF folder within the child theme
+ * to the ACF load paths variable
+ * @author Jim Barnes
+ * @since 1.0.0
+ * @param array $paths The path array
+ * @return array
+ */
+function add_acf_load_path( $paths ) {
+	unset( $paths[0] );
+
+	$paths[] = get_stylesheet_directory() . '/dev/acf';
+
+	return $paths;
+}
+
+add_filter( 'acf/settings/load_json', __NAMESPACE__ . '\add_acf_load_path', 10, 1 );
