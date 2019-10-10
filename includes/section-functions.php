c8b36ff6be935ac521d088256dd38c3468f78439
Jo Dickson <jogreybill@gmail.com>
2019-10-10 10:49:59 -0400

    WIP setting up individual list type markup; modified field names to reduce redundancy

--- includes/section-functions.php
+++ includes/section-functions.php
@@ -5,10 +5,11 @@
  */
 namespace FinAid\Theme\Includes\Sections;
 
+
 /**
- * Function for displaying the beginning
- * of a icon-list section
- * @author Jim Barnes
+ * Returns opening markup around a numbered list.
+ *
+ * @author Jo Dickson
  * @since 1.0.0
  * @param string $retval The unfiltered return value
  * @param WP_Post $section The section object
@@ -17,98 +18,237 @@ namespace FinAid\Theme\Includes\Sections;
  * @param string $section_id The section id to use
  * @return string The output
  */
-function icon_list_before( $retval, $section, $class, $title, $section_id ) {
-	$layout = get_field( 'finaid_section_layout', $section->ID );
-	// If this isn't an icon-list, return.
-	if ( $layout !== 'icon-list' ) return $retval;
-
-	$has_content = ! empty( $section->post_content );
-
-	ob_start();
-?>
-	<dl class="icon-list">
-<?php
-	$output = ob_get_clean();
-
-	if ( $has_content ) {
-		$retval .= apply_filters( 'the_content', $section->post_content ) . $output;
-	} else {
-		$retval = $output;
-	}
-
-	return $retval;
+function display_numbered_list_before( $retval, $section, $class, $title, $section_id ) {
+	return '<ol class="section-list section-numbered-list">';
 }
 
-add_filter( 'ucf_section_display_before', __NAMESPACE__ . '\icon_list_before', 11, 5 );
+/**
+ * Returns inner content of a numbered list.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string The output
+ */
+function display_numbered_list_content( $retval, $section ) {
+	return '';
+}
 
 /**
- * Function for displaying the beginning
- * of a icon-list section
+ * Returns the closing markup of a numbered list.
+ *
  * @author Jim Barnes
  * @since 1.0.0
  * @param string $retval The unfiltered return value
  * @param WP_Post $section The section object
  * @return string The output
  */
-function icon_list_content( $retval, $section ) {
-	$layout = get_field( 'finaid_section_layout', $section->ID );
-
-	// If this isn't an icon-list, return.
-	if ( $layout !== 'icon-list' ) return $retval;
-
-	$list = get_field( 'finaid_icon_list_fields', $section->ID );
-
-	ob_start();
-
-	if ( isset( $list['finaid_icon_list_items'] ) && count( $list['finaid_icon_list_items'] ) > 0 ) :
-		foreach( $list['finaid_icon_list_items'] as $item ) :
-?>
-	<dt class="align-self-center mb-2">
-		<span class="fa <?php echo $item['finaid_item_icon']; ?> fa-2x text-primary mr-2"></span>
-		<?php echo $item['finaid_item_label']; ?>
-	</dt>
-	<dd>
-		<?php echo apply_filters( 'the_content', $item['finaid_item_content'] ); ?>
-	</dd>
-<?php
-		endforeach;
-	endif;
-
-	return ob_get_clean();
+function display_numbered_list_after( $retval, $section ) {
+	return '</ol>';
 }
 
-add_filter( 'ucf_section_display', __NAMESPACE__ . '\icon_list_content', 10, 2 );
 
 /**
- * Function for displaying the beginning
- * of a icon-list section
+ * Returns opening markup around an icon list.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @param string $class The class to use when displaying the section
+ * @param string $title The title the display
+ * @param string $section_id The section id to use
+ * @return string The output
+ */
+function display_icon_list_before( $retval, $section, $class, $title, $section_id ) {
+	return '<ul class="section-list section-icon-list">';
+}
+
+/**
+ * Returns inner content of an icon list.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string The output
+ */
+function display_icon_list_content( $retval, $section ) {
+	return '';
+}
+
+/**
+ * Returns the closing markup of an icon list.
+ *
  * @author Jim Barnes
  * @since 1.0.0
  * @param string $retval The unfiltered return value
  * @param WP_Post $section The section object
  * @return string The output
  */
-function icon_list_after( $retval, $section ) {
-	$layout = get_field( 'finaid_section_layout', $section->ID );
+function display_icon_list_after( $retval, $section ) {
+	return '</ul>';
+}
 
-	// If this isn't an icon-list, return.
-	if ( $layout !== 'icon-list' ) return $retval;
 
-	$has_content = ! empty( $section->post_content );
+/**
+ * Returns opening markup around a checklist.
+ *
+ * @since 1.0.0
+ * @author Jo Dickson
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @param string $class The class to use when displaying the section
+ * @param string $title The title the display
+ * @param string $section_id The section id to use
+ * @return string HTML markup
+ */
+function display_checklist_before( $retval, $section, $class, $title, $section_id ) {
+	return '<ul class="section-list section-checklist">';
+}
 
-	ob_start();
-?>
-	</dl>
-<?php
-	$output = ob_get_clean();
+/**
+ * Returns inner content of a checklist.
+ *
+ * @author Jo Dickson
+ * @since 1.0.0
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string The output
+ */
+function display_checklist_content( $retval, $section ) {
+	return '';
+}
 
-	if ( $has_content ) {
-		$retval .= $output;
-	} else {
-		$retval = $output;
+/**
+ * Returns the closing markup of a checklist.
+ *
+ * @author Jim Barnes
+ * @since 1.0.0
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string The output
+ */
+function display_checklist_after( $retval, $section ) {
+	return '</ul>';
+}
+
+
+/**
+ * Returns opening markup around a section, depending on the
+ * given section's layout.
+ *
+ * @since 1.0.0
+ * @author Jo Dickson
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @param string $class The class to use when displaying the section
+ * @param string $title The title the display
+ * @param string $section_id The section id to use
+ * @return string HTML markup
+ */
+function display_section_before( $retval, $section, $class, $title, $section_id ) {
+	$layout = get_field( 'section_layout', $section );
+	switch ( $layout ) {
+		case 'list':
+			$list_type = get_field( 'section_layout', $section );
+			switch ( $list_type ) {
+				case 'numbered-list':
+					$retval = display_numbered_list_before( $retval, $section, $class, $title, $section_id );
+					break;
+				case 'icon-list':
+					$retval = display_icon_list_before( $retval, $section, $class, $title, $section_id );
+					break;
+				case 'checklist':
+				default:
+					$retval = display_checklist_before( $retval, $section, $class, $title, $section_id );
+					break;
+			}
+			break;
+		case 'default':
+		default:
+			break;
 	}
 
 	return $retval;
 }
 
-add_filter( 'ucf_section_display_after', __NAMESPACE__ . '\icon_list_after', 10, 2 );
+add_filter( 'ucf_section_display_before', __NAMESPACE__ . '\display_section_before', 11, 5 );
+
+
+/**
+ * Returns inner section markup, depending on the given section's layout.
+ *
+ * @since 1.0.0
+ * @author Jo Dickson
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string HTML markup
+ */
+function display_section_content( $retval, $section ) {
+	$layout = get_field( 'section_layout', $section );
+	switch ( $layout ) {
+		case 'list':
+			$list_type = get_field( 'section_layout', $section );
+			switch ( $list_type ) {
+				case 'numbered-list':
+					$retval = display_numbered_list_content( $retval, $section );
+					break;
+				case 'icon-list':
+					$retval = display_icon_list_content( $retval, $section );
+					break;
+				case 'checklist':
+				default:
+					$retval = display_checklist_content( $retval, $section );
+					break;
+			}
+			break;
+		case 'default':
+		default:
+			break;
+	}
+
+	return $retval;
+}
+
+add_filter( 'ucf_section_display', __NAMESPACE__ . '\display_section_content', 11, 2 );
+
+
+/**
+ * Returns closing markup around a section, depending on the
+ * given section's layout.
+ *
+ * @since 1.0.0
+ * @author Jo Dickson
+ * @param string $retval The unfiltered return value
+ * @param WP_Post $section The section object
+ * @return string HTML markup
+ */
+function display_section_after( $retval, $section ) {
+	$layout = get_field( 'section_layout', $section );
+	switch ( $layout ) {
+		case 'list':
+			$list_type = get_field( 'section_layout', $section );
+			switch ( $list_type ) {
+				case 'numbered-list':
+					$retval = display_numbered_list_after( $retval, $section );
+					break;
+				case 'icon-list':
+					$retval = display_icon_list_after( $retval, $section );
+					break;
+				case 'checklist':
+				default:
+					$retval = display_checklist_after( $retval, $section );
+					break;
+			}
+			break;
+		case 'default':
+		default:
+			break;
+	}
+
+	return $retval;
+}
+
+add_filter( 'ucf_section_display_after', __NAMESPACE__ . '\display_section_after', 11, 2 );
